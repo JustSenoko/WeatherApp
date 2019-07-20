7f40b4b7d6229bdf6e1e045cd277d5345314a72e
JustSenoko <just.senoko@gmail.com>
2019-07-20 11:19:53 +0300

    Переименована иконка поиска, чтобы не было ошибки компиляции

--- app/src/main/java/com/example/weatherapp/MainActivity.java
+++ app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -59,6 +59,7 @@ public class MainActivity extends AppCompatActivity {
             super.onActivityResult(requestCode, resultCode, data);
             return;
         }
+
         updateView();
     }
 
