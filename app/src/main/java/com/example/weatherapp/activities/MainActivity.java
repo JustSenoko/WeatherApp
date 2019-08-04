347750e8999646e6101d8fa76c5b240644fabb3a
JustSenoko <just.senoko@gmail.com>
2019-08-04 19:45:22 +0300

    боковая навигация

--- app/src/main/java/com/example/weatherapp/activities/MainActivity.java
+++ app/src/main/java/com/example/weatherapp/activities/MainActivity.java
@@ -6,6 +6,11 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.FragmentManager;
 
 import com.example.weatherapp.R;
@@ -18,12 +23,14 @@ import com.example.weatherapp.models.pojo.City;
 import com.example.weatherapp.utils.ConfSingleton;
 import com.example.weatherapp.utils.Publisher;
 import com.example.weatherapp.utils.UserPreferences;
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends BaseActivity
-        implements CitySelectionFragment.OnFragmentCitySelectionListener,
-            MainFragment.OnMainFragmentListener,
-            SettingsFragment.OnSettingsFragmentListener,
-            PublishGetter {
+         implements NavigationView.OnNavigationItemSelectedListener,
+                    CitySelectionFragment.OnFragmentCitySelectionListener,
+                    MainFragment.OnMainFragmentListener,
+                    SettingsFragment.OnSettingsFragmentListener,
+                    PublishGetter {
     private Publisher publisher = new Publisher();
     private UserPreferences userPreferences;
     private SelectedCities selectedCities;
@@ -37,8 +44,21 @@ public class MainActivity extends BaseActivity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        init();
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+        navigationView.setNavigationItemSelectedListener(this);
+
+        init();
+
         if (savedInstanceState==null) {
             fragmentManager.beginTransaction()
                     .add(R.id.fragment, mainFragment)
@@ -62,8 +82,40 @@ public class MainActivity extends BaseActivity
     }
 
     @Override
-    public boolean onCreatePanelMenu(int featureId, Menu menu) {
+    public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.main_activity_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.menu_change_city) {
+            openCitySelectionFragment();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_settings) {
+            openSettingsFragment();
+        } else if (id == R.id.nav_share) {
+            //TODO
+            Toast.makeText(this, getResources().getString(R.string.coming_soon), Toast.LENGTH_SHORT).show();
+        } else if (id == R.id.nav_feedback) {
+            //TODO
+            Toast.makeText(this, getResources().getString(R.string.coming_soon), Toast.LENGTH_SHORT).show();
+        } else if (id == R.id.nav_about) {
+            //TODO
+            Toast.makeText(this, getResources().getString(R.string.coming_soon), Toast.LENGTH_SHORT).show();
+        }
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
         return true;
     }
 
@@ -77,7 +129,6 @@ public class MainActivity extends BaseActivity
         userPreferences.setCurrentCityId(city.getId());
         selectedCities.setCurrentCity(city);
         fragmentManager.popBackStack();
-        //publisher.notifyUpdateWeatherInfo();
     }
 
     @Override
@@ -87,10 +138,16 @@ public class MainActivity extends BaseActivity
 
     @Override
     public void onBackPressed() {
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+            return;
+        }
         if (back_pressed + 2000 > System.currentTimeMillis()) {
             finish();
         } else {
             Toast.makeText(getBaseContext(), getResources().getString(R.string.press_again_to_exit), Toast.LENGTH_SHORT).show();
+            super.onBackPressed();
         }
         back_pressed = System.currentTimeMillis();
     }
@@ -108,8 +165,11 @@ public class MainActivity extends BaseActivity
             .commit();
     }
 
-    public void changeCity(MenuItem item) {
-        openCitySelectionFragment();
+    public void openSettingsFragment() {
+        fragmentManager.beginTransaction()
+                .replace(R.id.fragment, settingsFragment)
+                .addToBackStack("")
+                .commit();
     }
 
     @Override
