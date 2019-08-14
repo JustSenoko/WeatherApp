bd1ffd36e13d7db035e97871f4ef54d4bb15c9b4
JustSenoko <just.senoko@gmail.com>
2019-08-14 19:38:40 +0300

    удален неиспользуемый код

--- app/src/main/java/com/example/weatherapp/models/pojo/City.java
+++ app/src/main/java/com/example/weatherapp/models/pojo/City.java
@@ -21,24 +21,6 @@ public class City implements Serializable {
         this.country = null;
     }
 
-//    protected City(Parcel in) {
-//        name = in.readString();
-//        id = in.readInt();
-//        country = in.readString();
-//    }
-
-//    public static final Creator<City> CREATOR = new Creator<City>() {
-//        @Override
-//        public City createFromParcel(Parcel in) {
-//            return new City(in);
-//        }
-//
-//        @Override
-//        public City[] newArray(int size) {
-//            return new City[size];
-//        }
-//    };
-
     public String getName() {
         return name;
     }
@@ -65,16 +47,4 @@ public class City implements Serializable {
     public int hashCode() {
         return Objects.hash(name, id);
     }
-
-//    @Override
-//    public int describeContents() {
-//        return 0;
-//    }
-//
-//    @Override
-//    public void writeToParcel(Parcel dest, int flags) {
-//        dest.writeString(name);
-//        dest.writeInt(id);
-//        dest.writeString(country);
-//    }
 }
